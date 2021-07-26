c38f92cde873bb83cee691cf99b38e401de78448
Ashenika <ashenikaperera@gmail.com>
2021-07-26 15:08:56 +0530

    PQ-5 : Add a function to print a number

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ class SimpleClass
 {
     // property declaration
     public string $name = 'Hi My Name is Ash';
+    public int $number = 25;
 
     // method declaration
     public function displayVar(): void
@@ -11,5 +12,11 @@ class SimpleClass
         for ($count = 0; $count <= 10; $count++) {
             echo $this->name;
         }
+    }
+
+    // method declaration
+    public function displayNum(): void
+    {
+        echo "This is a Number" . $this->number;
     }
 }
